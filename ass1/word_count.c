c0f6ecab5f8ba31868434f6c212bab7e0219b953
boscoj24@mail.wlu.edu <boscoj24@mail.wlu.edu>
2024-01-20 18:31:42 -0500

    stops at first newline

--- ass1/word_count.c
+++ ass1/word_count.c
@@ -2,23 +2,54 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFER_SIZE 8
+#include <stdint.h>
+#define BUFFER_SIZE 2048
+#define WORD_SIZE 64
+
+void cleanStr(char * str){
+	int i, j = 0;
+    for (i = 0; str[i]; i++) {
+        if (isalpha(str[i])) {
+            str[j++] = tolower(str[i]);
+        }
+    }
+    str[j] = '\0';
+}
 
 int main(int argc, char** argv){
 
     char buffer[BUFFER_SIZE];
-
-    FILE* file = fopen("hello.txt", "r");
-    fgets(buffer, sizeof(buffer), file);
-    fclose(file);
-
-    printf("Number of args: %d\n", argc);
-    int i = 0;
-    while (i < argc){
-        printf("Arg %d: %s\n", i, argv[i]);
-        i++;
-    }
-
-    printf("Text: %s\n", buffer);
+	char *fname;
+	char *word;
+	
+    // store the file name as the first cmd-line arg
+    if (argc > 1) {
+		fname = argv[1];
+    } else {
+		fname = "hello.txt";
+	}
+	if (argc > 2) {
+		word = argv[2];
+	} else {
+		word = "hello";
+	}
+    FILE* file = fopen(fname, "r");
+	int count = 0;
+	char* tok;
+	while(fgets(buffer, sizeof(buffer), file)!=NULL){
+		//fclose(file);
+		//count occurances
+		tok = strtok(buffer, " ,.-!?;:\"'\n\t");
+		while (tok != NULL){
+			cleanStr(tok);
+			printf(">%s\n", tok);
+			if (strcmp(tok, word)==0){
+				count++;
+			}
+			tok = strtok(NULL, " ,.-!?;:\"'\n\t");
+		}
+	}
+	printf("%s count: %d\n\n", word, count);
+	fclose(file);
     return (intptr_t) &main;
 }
