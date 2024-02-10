9726d7c2d6b8e1969767175e3cc917e343137eb3
JackBosco <boscoj24@mail.wlu.edu>
2024-02-10 17:53:32 -0500

    seg fault

--- ass3/monte_par.c
+++ ass3/monte_par.c
@@ -7,18 +7,22 @@ Do the monte carlo estimation sequentially
 #include <stdint.h>
 #include <time.h>
 #include <math.h>
+#include <pthread.h>
 
-#define N_SEQ 1000000
+#define N_SEQ 1000000;
+#define N_THREADS 10;
+
 int n_seq;
-double* randoms_x;
-double* randoms_y;
+int n_threads;
+double* results;
 
-double find_pi(){
-	srand(time(NULL)); // seed the random numbers
+void* find_pi(void* thread_num){
     // Do the sequence for monte carlo
 	// 1. Randomly sample many points, (x, y) pairs, with each coordinate uniformly drawn between [−1, 1].
 	int count = 0;
-	for (int i = 0; i<n_seq; i++){
+	intptr_t tn = (intptr_t)thread_num;
+	srand(time((time_t*) 42 + tn)); // 42 is the answer to everthing
+	for (int i = tn * (n_seq/n_threads); i<(tn+1) * (n_seq/n_threads); i++){
 		double random_x = (double)rand() / (double)(RAND_MAX/2.0) - 1.0; // I stole this :)
 		double random_y = (double)rand() / (double)(RAND_MAX/2.0) - 1.0; // https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
 
@@ -37,7 +41,8 @@ double find_pi(){
 	double ratio = (double)count / (double)n_seq;
 
 	// 5. Multiply by 4 to obtain π.
-    return ratio * 4;
+    results[tn] = ratio;
+	return 0;
 }
 
 int main(int argc, char** argv){
@@ -45,15 +50,34 @@ int main(int argc, char** argv){
     double time_diff;
 
 	//parse command line args
-	if (argc > 1) {
+		// get command line args
+	if (argc > 2) {
 		n_seq = atoi(argv[1]);
+		n_threads = atoi(argv[2]);
 	} else {
 		n_seq = N_SEQ;
+		n_threads = N_THREADS;
 	}
-	randoms_x = malloc(n_seq * sizeof(double));
-	randoms_y = malloc(n_seq * sizeof(double));
+	
+	pthread_t* HANDLERS = malloc(n_threads * sizeof(pthread_t)); // malloc thread handlers
+	results = malloc(n_threads * sizeof(double)); // malloc result handlers
+    
     clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
-    double pi = find_pi();
+	for (int i = 0; i<n_threads; i++){
+		intptr_t e = i;
+		pthread_create(&HANDLERS[i], NULL, find_pi, (void*)e);
+	}
+	//Waiting!
+	for (int64_t i = 0; i<n_threads; i++){
+		pthread_join(HANDLERS[i], NULL);
+	}	
+	double pi = 0.0;
+	for (int64_t i = 0; i<n_threads; i++){
+		pi += results[i];
+	}
+	pi *= 4 * 4;
+	free(HANDLERS);
+	free(results);
     clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
 
     time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
