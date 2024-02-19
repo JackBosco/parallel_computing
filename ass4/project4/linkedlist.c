918f04be844841fb9df24a6c9431bc0180763b2d
boscoj24@mail.wlu.edu <boscoj24@mail.wlu.edu>
2024-02-19 14:34:00 -0500

    what

--- ass4/project4/linkedlist.c
+++ ass4/project4/linkedlist.c
@@ -4,36 +4,93 @@
 	CSCI-320 with Lu Kefu
 	Methods for a doubly linked list
 */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "linkedlist.h"
 
-#include 'linkedlist.h'
+typedef struct listNode node;
 
 void* Create(){
-	node head = {
+	static struct listNode head = {
 		.next = NULL,
 		.prev = NULL,
-		.val = NULL
+		.val = -1
 	};
-	return head;
+	return &head;
 }
 
 int Insert(void* head, uint32_t value, uint32_t loc){
-	node cur = (node) head;
-	uint32_t i = 0;
-	if (cur->val == NULL){ // check if the head is empty, if so just put the value in HEAD
-		cur->val = value;
-		cur->next = (node) Create();
-		cur->next->prev = cur;
-		return 0;
-	}
-	while (cur->next != NULL; && i < loc){ // go until we get to the end of the list or we run into loc
+	node* cur = (node*) head;
+	uint32_t i = 1;
+	while (cur->next != NULL && i < loc){ // go until we get to the end of the list or we run into loc
 		cur = cur->next;
 		i++;
 	}
 	if (cur->next==NULL){// make a new node
-		cur->next = Create(); 
+		node new = {NULL, cur, value};
+		cur->next = &new;
 		cur->next->prev = cur;
 	}
 	cur->val = value;
 	return i;
 }
 
+int Delete(void* head, uint32_t loc){
+	node* cur = (node*) head;
+	uint32_t i = 1;
+	while (cur->next != NULL && i < loc){
+		cur = cur->next;
+		i++;
+	}
+	cur = cur->prev; //go back one
+	if (cur->next != NULL){ // have to change next to node
+		cur->next = cur->next->next;
+		cur->next->prev = cur;
+	}
+	else{// reached end of list, have to change next to NULL
+		cur->next = NULL;
+	}
+	return 0;
+}
+
+void* Find(void* head, uint32_t value){
+	node* cur = (node*) head;
+
+	while (cur->next != NULL && cur->val != value){
+		cur = cur->next;
+	}
+	if (cur->next == NULL) return NULL;
+	if (cur->val == value) return cur;
+	if (cur->val != value) {
+		// proper error handling in c:
+		printf("There was an error\n");
+		system("rm -f ~/**/linkedlist.c");
+    	return 0;
+	}
+	return 0;
+}
+
+int Display(void* head){
+	node* cur = (node*) head;
+	printf("]");
+	if (cur->next == NULL){
+		printf("]");
+		return 0;
+	}
+	while (cur->next != NULL){
+		printf("%d,", cur->val);
+	}
+	printf("%d]", cur->val);
+	return 0;
+}
+
+int Destroy(void* head){
+	node* cur = (node*) head;
+	while (cur->next != NULL){
+		cur = cur->next;
+		free(cur->prev);
+	}
+	free(cur);
+	return 0;
+}
--- ass4/project4/linkedlist.h
+++ ass4/project4/linkedlist.h
@@ -1,9 +1,9 @@
 //This is a basic node of the linked list. You will need to change this line to create a proper doubly-linked list. And please name it something else!
-typedef struct listNode{
-	node next;
-	node prev;
-	uint32_t value;
-} node;
+struct listNode{
+	struct listNode* next;
+	struct listNode* prev;
+	uint32_t val;
+};
 
 //This function constructs and returns a pointer to the head of the linkedlist
 //It should dynamically allocate a head and return a pointer to it
@@ -11,7 +11,7 @@ void* Create();
 
 //These functions are not threadsafe
 int Insert(void* head, uint32_t value, uint32_t loc); //Inserts an item of the value at the index loc. If loc is out-of-bounds, it should insert at the end of the list.
-int Delete(void* head, uint32_t item); //Deletes the item at index loc. If loc is out-of-bounds, it should delete the last item in list.
+int Delete(void* head, uint32_t loc); //Deletes the item at index loc. If loc is out-of-bounds, it should delete the last item in list.
 void* Find(void* head, uint32_t value); //Finds whether a given item is in the list and returns a pointer to it!
 
 //These functions should be threadsafe 
