67ea354fdd83fe15c5f23f47441cf8d802d2f594
boscoj24@mail.wlu.edu <boscoj24@mail.wlu.edu>
2024-02-19 17:24:20 -0500

    finished p1

--- ass4/project4/linkedlist.c
+++ ass4/project4/linkedlist.c
@@ -12,57 +12,56 @@
 typedef struct listNode node;
 
 void* Create(){
-	static struct listNode head = {
-		.next = (void*) NULL,
-		.prev = (void*) NULL,
-		.val = -1
-	};
-	return &head;
+	node* head = (node*) malloc(sizeof(node));
+	head->prev = NULL;
+	head->val = -1;
+	node* tail = (node*) malloc(sizeof(node));
+	tail->prev = head;
+	head->next = tail;
+	tail->val = -1;
+	tail->next = NULL;
+	return head;
 }
+
 
 int Insert(void* head, uint32_t value, uint32_t loc){
 	node* cur = (node*) head;
-	uint32_t i = 1;
-
-	while (cur->next && i < loc){ // go until we get to the end of the list or we run into loc
+	uint32_t i = 0;
+	while (cur->next && i <= loc){ // go until we get to the end of the list or we run into loc
 		cur = cur->next;
 		i++;
 	}
-	if (cur->next==NULL){// make a new node
-		node new = {NULL, cur, value};
-		cur->next = &new;
-		cur->next->prev = cur;
-	}
-	cur->val = value;
+	node* new = (node*) malloc(sizeof(node));
+	new->next = cur;
+	new->prev = cur->prev;
+	new->val = value;
+	cur->prev=new;
+	new->prev->next=new;
 	return i;
 }
 
 int Delete(void* head, uint32_t loc){
 	node* cur = (node*) head;
-	uint32_t i = 1;
-	while (cur->next != NULL && i < loc){
+	uint32_t i = 0;
+	while (cur->next->next && i < loc){
 		cur = cur->next;
 		i++;
 	}
 	cur = cur->prev; //go back one
-	if (cur->next != NULL){ // have to change next to node
-		cur->next = cur->next->next;
-		cur->next->prev = cur;
-	}
-	else{// reached end of list, have to change next to NULL
-		cur->next = NULL;
-	}
+	cur->next = cur->next->next;
+	free(cur->next->prev);
+	cur->next->prev = cur;
 	return 0;
 }
 
 void* Find(void* head, uint32_t value){
 	node* cur = (node*) head;
 
-	while (cur->next != NULL && cur->val != value){
+	while (cur->next && cur->val != value){
+		if (cur->val == value) return cur;
 		cur = cur->next;
 	}
-	if (cur->next == NULL) return NULL;
-	if (cur->val == value) return cur;
+	return cur;
 	if (cur->val != value) {
 		// proper error handling in c:
 		printf("There was an error\n");
@@ -74,21 +73,22 @@ void* Find(void* head, uint32_t value){
 
 int Display(void* head){
 	node* cur = (node*) head;
-	printf("]");
-	if (cur->next == NULL){
-		printf("]");
+	printf("[");
+	if (!cur->next->next){
+		printf("]\n");
 		return 0;
 	}
-	while (cur->next != NULL){
-		printf("%d,", cur->val);
+	while (cur->next->next){
+		if (cur->val != -1) printf("%d,", cur->val);
+		cur = cur->next;
 	}
-	printf("%d]", cur->val);
+	printf("%d]\n", cur->val);
 	return 0;
 }
 
 int Destroy(void* head){
 	node* cur = (node*) head;
-	while (cur->next != NULL){
+	while (cur->next){
 		cur = cur->next;
 		free(cur->prev);
 	}
