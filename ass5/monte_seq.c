783085bd97071ede8689650d4f2e530f75626ac8
boscoj24@mail.wlu.edu <boscoj24@mail.wlu.edu>
2024-03-20 11:33:03 -0400

    init part1

---
+++ ass5/monte_seq.c
@@ -0,0 +1,65 @@
+/* 
+Jack Bosco
+Do the monte carlo estimation sequentially
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <math.h>
+
+#define N_SEQ 1000000
+int n_seq;
+double* randoms_x;
+double* randoms_y;
+
+double find_pi(){
+	srand(time(NULL)); // seed the random numbers
+    // Do the sequence for monte carlo
+	// 1. Randomly sample many points, (x, y) pairs, with each coordinate uniformly drawn between [−1, 1].
+	int count = 0;
+	for (int i = 0; i<n_seq; i++){
+		double random_x = (double)rand() / (double)(RAND_MAX/2.0) - 1.0; // I stole this :)
+		double random_y = (double)rand() / (double)(RAND_MAX/2.0) - 1.0; // https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
+
+		// 2. Count the number of points which lie within the circle
+		double dist = sqrt(pow(random_x, 2.0) + pow(random_y, 2.0));
+		if (random_x >= 0.0 
+			&& random_y >= 0.0 
+			&& dist <= 1.0) {
+				count++;
+			}
+	}
+	// 3. Count the total number of points which lie within the square (all of them)
+	// n_seq
+
+	// 4. Divide the two, this should be proportional to the ratio of the areas, which is π
+	double ratio = (double)count / (double)n_seq;
+
+	// 5. Multiply by 4 to obtain π.
+    return ratio * 4 * 4;
+}
+
+int main(int argc, char** argv){
+    struct timespec start, end; //structs used for timing purposes, it has two memebers, a tv_sec which is the current second, and the tv_nsec which is the current nanosecond.
+    double time_diff;
+
+	//parse command line args
+	if (argc > 1) {
+		n_seq = atoi(argv[1]);
+	} else {
+		n_seq = N_SEQ;
+	}
+	randoms_x = malloc(n_seq * sizeof(double));
+	randoms_y = malloc(n_seq * sizeof(double));
+    clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
+    double pi = find_pi();
+    clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
+
+    time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
+    time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; //Difference in nanoseconds
+
+    printf("The time taken is %f \n", time_diff);
+    printf("The number of terms is %d\n", n_seq);
+    printf("Pi is %.20lf\n", pi);
+}
---
+++ ass5/pi_seq.c
@@ -0,0 +1,43 @@
+/* 
+Jack Bosco
+This is the first part of assignment 3, using sequential coding for the pi estimation
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <math.h>
+
+#define N_SEQ 1000000;
+
+double find_pi(int n){
+    //use the sequene to determine Pi from the amount of repitions passed in
+	double Pi = 0;
+	for (int i = 0; i<n; i++){
+		Pi += pow(-1.0, (double)i) / (2*i + 1);
+	}
+    return 4 * Pi; 
+}
+
+int main(int argc, char** argv){
+    struct timespec start, end; //structs used for timing purposes, it has two memebers, a tv_sec which is the current second, and the tv_nsec which is the current nanosecond.
+    double time_diff;
+	int n;
+	// get command line args
+	if (argc > 1) {
+		n = atoi(argv[1]);
+	} else {
+		n = N_SEQ;
+	}
+    
+    clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
+    double pi = find_pi(n);
+    clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
+
+    time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
+    time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; //Difference in nanoseconds
+
+    printf("The time taken is %f \n", time_diff);
+    printf("The number of terms in the sequence is %d \n", n);
+    printf("Pi is %.20lf\n", pi);
+}
